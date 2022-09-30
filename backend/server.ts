4f066e7d12e5e227500c8ac86c80a72f2e4e564e
KEENTO <handball0321@gmail.com>
2022-09-30 09:55:17 -0700

    Initial commit

---
+++ backend/server.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Home");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: server is running on port ${port}.`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Costco</h1>
+    </div>
+  );
+}
+
+export default App;
